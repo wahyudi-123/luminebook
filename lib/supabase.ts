675d7e041c1118659428c601e2a0430938daf780
rizkyud123 <rw070600@gmail.com>
2025-11-21 10:31:21 +0700

    Hapus semua file lama

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://kizzkyolujiukokrzvte.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpenpreW9sdWppdWtva3J6dnRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2MjkzMzIsImV4cCI6MjA3OTIwNTMzMn0.tLmgnpNGPHvWHaQW7-pDDfSpWj6OMEDWlZfNMN71vAc';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
